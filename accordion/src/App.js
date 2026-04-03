33b4e5ff965ae48baa92d5eff4de2c5e1e361848
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-03 19:06:34 +0545

    added accordion

---
+++ accordion/src/App.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "./styles.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItems
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i}
+          key={el.title}
+        >
+          {el.text}
+        </AccordionItems>
+      ))}
+    </div>
+  );
+}
+
+function AccordionItems({ num, title, curOpen, onOpen, children }) {
+  // const [isOpen, setIsOpen] = useState(false);
+  const isOpen = num === curOpen;
+  function handleToggle() {
+    // setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
+  }
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+
+      <p className="title"> {title}</p>
+      <p className="icon">{isOpen ? "-" : "+"} </p>
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
