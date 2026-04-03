ea7e0cf332414b5e9156f6fa3a74003ae234e23d
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-03 19:00:36 +0545

    Add travel-list as normal folder

---
+++ travel-list/src/components/PackingList.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Item from "./Item";
+export default function PackingList({
+  items,
+  onDeleteItems,
+  onToggleItem,
+  onClearItems,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            key={item.id}
+            item={item}
+            onDeleteItems={onDeleteItems}
+            onToggleItem={onToggleItem}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sorted By Input</option>
+          <option value="description">Sorted by Description</option>
+          <option value="packed">Sorted by Packed Status</option>
+        </select>
+        <button onClick={onClearItems}>clear items</button>
+      </div>
+    </div>
+  );
+}
