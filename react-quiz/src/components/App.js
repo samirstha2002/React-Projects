fceead91620d00b4aaaab2068228e425c3ca627f
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-13 15:41:31 +0545

    handling correct answer with points

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -11,6 +11,8 @@ const initialState = {
   //loading,error,ready,finished,'active'
   status: "loading",
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -21,6 +23,16 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? question.points + state.points
+            : state.points,
+      };
 
     default:
       throw new Error("Action unknown");
@@ -28,7 +40,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState,
   );
@@ -49,7 +61,13 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Questions question={questions[index]} />}
+        {status === "active" && (
+          <Questions
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
