bfa0ab61afc921cb49527f00baa9d6ecbffc528d
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-16 16:29:02 +0545

    created context for city page in worldwise website

---
+++ worldwide/src/contexts/CitiesContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect } from "react";
+
+const CitiesContext = createContext();
+
+const BASE_URL = "http://localhost:8000";
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (error) {
+        alert("There was problem while loading...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+export { CitiesProvider };
