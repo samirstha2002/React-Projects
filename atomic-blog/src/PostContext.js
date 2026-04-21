c766e0cf7dc250facda5790ddee4337c5f955f43
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-21 12:00:51 +0545

    useMemo and useCallback

--- atomic-blog/src/PostContext.js
+++ atomic-blog/src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 
 import { faker } from "@faker-js/faker";
 
@@ -35,20 +35,18 @@ function PostProvider({ children }) {
   function handleClearPosts() {
     setPosts([]);
   }
-
+  const value = useMemo(() => {
+    return {
+      posts: searchedPosts,
+      onAddPost: handleAddPost,
+      onClearPosts: handleClearPosts,
+      searchQuery,
+      setSearchQuery,
+    };
+  }, [searchQuery, searchedPosts]);
   return (
     //Provide value to child component
-    <PostContext.Provider
-      value={{
-        posts: searchedPosts,
-        onAddPost: handleAddPost,
-        onClearPosts: handleClearPosts,
-        searchQuery,
-        setSearchQuery,
-      }}
-    >
-      {children}
-    </PostContext.Provider>
+    <PostContext.Provider value={value}>{children}</PostContext.Provider>
   );
 }
 function usePosts() {
