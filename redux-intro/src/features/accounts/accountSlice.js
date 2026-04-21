8685bca3c6d01700189d84c1ea435af47fff0a1a
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-21 18:31:12 +0545

    customer slice using reduxtoolkit

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance = state.balance + action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance = state.balance - action.payload;
@@ -31,16 +32,39 @@ const accountSlice = createSlice({
       },
     },
 
-    payLoan(state, action) {
+    payLoan(state) {
       state.balance -= state.loan;
       state.loan = 0;
       state.loanPurpose = "";
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
 
-export const { deposit, withdraw, requestLoan, payLoan } = accountSlice.actions;
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
 
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    try {
+      const res = await fetch(
+        `/latest?amount=${amount}&from=${currency}&to=USD`,
+      );
+
+      if (!res.ok) throw new Error("API failed");
+
+      const data = await res.json();
+      const converted = data.rates.USD;
+
+      dispatch({ type: "account/deposit", payload: converted });
+    } catch (err) {
+      console.error("Deposit failed:", err);
+    }
+  };
+}
 export default accountSlice.reducer;
 
 /*export default function accountReducer(state = initialStateAccount, action) {
