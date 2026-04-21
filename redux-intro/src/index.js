f1f1d58d892e58c8fa184d6d5ed94c334b178ef7
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-21 14:38:37 +0545

    intreacting redux with react using react-redux

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
 
@@ -9,6 +10,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
