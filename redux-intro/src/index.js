14e13ef46012919b56bde5ef96301393b0878f18
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-21 15:45:36 +0545

    dispatch action using useDispatch

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -5,8 +5,6 @@ import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store";
 
-store.dispatch({ type: "account/deposit", payload: 600 });
-console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
