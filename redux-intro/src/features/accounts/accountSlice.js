6dcac72e097bc5d5b066669fce6b76b21b8cf9f2
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-21 16:52:14 +0545

    making api call with redux thunk

--- redux-intro/src/features/accounts/accountSlice.js
+++ redux-intro/src/features/accounts/accountSlice.js
@@ -2,12 +2,17 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+        isLoading: false,
+      };
 
     case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
@@ -27,13 +32,34 @@ export default function accountReducer(state = initialStateAccount, action) {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
     default:
       return state;
   }
 }
 
-export function deposit(amt) {
-  return { type: "account/deposit", payload: amt };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    try {
+      const res = await fetch(
+        `/latest?amount=${amount}&from=${currency}&to=USD`,
+      );
+
+      if (!res.ok) throw new Error("API failed");
+
+      const data = await res.json();
+      const converted = data.rates.USD;
+
+      dispatch({ type: "account/deposit", payload: converted });
+    } catch (err) {
+      console.error("Deposit failed:", err);
+    }
+  };
 }
 export function withdraw(amt) {
   return { type: "account/withdraw", payload: amt };
