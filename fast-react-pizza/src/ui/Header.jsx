b50f6fae8a86e4c04e98c045097824b5e649a24c
samirstha2002 <154325590+samirstha2002@users.noreply.github.com>
2026-04-23 10:02:19 +0545

    flexbox and responsive design

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
-import UserName from "../features/user/UserName";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import UserName from '../features/user/UserName';
 
 function Header() {
   return (
-    <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200">
+    <header className="flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
